e4c99d98d744c26a1104c7a93fabe46da9caadb4
Calvin-77 <anggriadycalvin@gmail.com>
2025-09-04 16:53:56 +0800

    postgres database implemented

---
+++ src/utils/index.js
@@ -0,0 +1,19 @@
+const mapDBToModel = ({
+    id,
+    title,
+    body,
+    tags,
+    created_at,
+    updated_at
+}) => ({
+    id,
+    title,
+    body,
+    tags,
+    createdAt: created_at,
+    updatedAt: updated_at,
+})
+
+module.exports = { mapDBToModel };
+
+// mengubah nama created_at dan updated_at (nama kolom di database) menjadi createdAt dan updatedAt (nama variable di script) agar tidak terjadi error.
